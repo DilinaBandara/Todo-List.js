8be98b45a1dd0d06a81abc19f481e23eef319b34
Dilina Bandara <dilinaedu@gmail.com>
2023-01-18 04:37:08 +0530

    Complete todo list

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const addBtn = document.querySelector('.todo-button')
 const input = document.querySelector('.todo-input')
 const todoList = document.querySelector('.todo-list')
 const filter = document.querySelector('.filter-todo')
-
+const option = document.getElementsByTagName('option')
 
 addBtn.addEventListener('click', function(event){
   event.preventDefault()
@@ -21,7 +21,7 @@ addBtn.addEventListener('click', function(event){
 
 todoList.addEventListener('click', function(event){
   if(event.target.classList.contains('complete-btn')){
-    event.target.parentElement.classList.add('completed')
+    event.target.parentElement.classList.toggle('completed')
   }
 })
 
@@ -30,3 +30,30 @@ todoList.addEventListener('click', function(event){
     event.target.parentElement.classList.add('deleted')
   }
 })
+
+// Check this code and go through the stuff you dont understand
+filter.addEventListener('change', function(){
+  let selectedOption = filter.value;
+  let todos = Array.from(todoList.children);
+  todos.forEach(function(todo){
+      switch(selectedOption){
+          case "completed":
+              if(todo.classList.contains('completed')){
+                  todo.style.display = "block";
+              }else{
+                  todo.style.display = "none";
+              }
+              break;
+          case "incomplete":
+              if(!todo.classList.contains('completed')){
+                  todo.style.display = "block";
+              }else{
+                  todo.style.display = "none";
+              }
+              break;
+          case "all":
+              todo.style.display = "block";
+              break;
+      }
+  });
+});
