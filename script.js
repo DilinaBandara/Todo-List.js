1b0b613028c6b55f1992acf2d0e93f271deb877b
Dilina Bandara <dilinaedu@gmail.com>
2023-01-18 04:10:46 +0530

    Working on complete and trash buttons

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ const filter = document.querySelector('.filter-todo')
 
 addBtn.addEventListener('click', function(event){
   event.preventDefault()
-  const listElement = document.createElement('li')
-  listElement.textContent = input.value
-  todoList.appendChild(listElement)
+  if(input.value.trim() === ''){
+    window.alert("Task cannot be empty");
+  } else{
+    let listTemplate = `<li>${input.value}
+    <button class="trash-btn">Trash</button>
+    <button class="complete-btn">Complete</i></button>
+    </li>
+    `
+    todoList.insertAdjacentHTML('beforeend', listTemplate)
+    input.value = ""
+  }
+})
+
+todoList.addEventListener('click', function(event){
+  if(event.target.classList.contains('complete-btn')){
+    event.target.parentElement.classList.add('completed')
+  }
+})
+
+todoList.addEventListener('click', function(event){
+  if(event.target.classList.contains('trash-btn')){
+    event.target.parentElement.classList.add('deleted')
+  }
 })
